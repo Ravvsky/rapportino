b8ba9a623b9c3913f628ded0b38eb9e91f96ceb3
Ravvsky <pawel.rawsk@gmail.com>
2024-03-26 00:14:47 +0100

    chore:remove comments

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,6 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const currentUser = request.cookies.get("user")?.value;
-  console.log("user: ", currentUser, "req url:", request.nextUrl.pathname);
   if (currentUser && request.nextUrl.pathname.startsWith("/login")) {
     return Response.redirect(new URL("/overview", request.url));
   }
