f496d2731f76d91850373953eff29db35ee0c66f
Ravvsky <pawel.rawsk@gmail.com>
2024-05-05 19:35:08 +0200

    fix: adjust handle login to work with new encrypt

--- app/_actions/handleLogin.ts
+++ app/_actions/handleLogin.ts
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 import { encrypt } from "../_utils/crypto";
 import { redirect } from "next/navigation";
 export async function handleLogin(sessionData: string) {
-  const encryptedSessionData = encrypt(sessionData); // Encrypt your session data
+  const encryptedSessionData = await encrypt(sessionData);
   cookies().set("user", encryptedSessionData, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "development",
