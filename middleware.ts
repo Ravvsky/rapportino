feabf42dc2cbd2e7c3f0fff44cf190caf5917969
Ravvsky <pawel.rawsk@gmail.com>
2024-05-11 18:18:31 +0200

    feat: update middleware to work with login with google

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,15 @@
 import type { NextRequest } from "next/server";
+import { auth } from "./auth";
 
-export function middleware(request: NextRequest) {
-  const currentUser = request.cookies.get("user")?.value;
-  if (currentUser && request.nextUrl.pathname.startsWith("/login")) {
+export async function middleware(request: NextRequest) {
+  const OAuthSession = await auth();
+  const currentUser = request.cookies.get("user")?.value || OAuthSession;
+
+  if (
+    currentUser &&
+    (request.nextUrl.pathname.startsWith("/login") ||
+      request.nextUrl.pathname.startsWith("/sign-up"))
+  ) {
     return Response.redirect(new URL("/overview", request.url));
   }
   if (currentUser && request.nextUrl.pathname === "/") {
