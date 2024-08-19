7e8e6b3d18b188bdf32cc4f8739d35b1d14de35c
Ravvsky <pawel.rawsk@gmail.com>
2024-08-19 23:20:19 +0200

    feat: update dropdown with list of team member roles

--- app/teams/[slug]/members/page.tsx
+++ app/teams/[slug]/members/page.tsx
@@ -2,16 +2,26 @@ import { getTeamByID } from "@/app/_services/teamServices/getTeamByID";
 import { notFound } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import sendEmail from "@/app/_actions/sendEmail";
+import AddMemberDialog from "../../_components/AddMemberDialog";
+import getLoggedUserID from "@/app/_actions/getLoggedUserID";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const Page = async ({ params }: { params: { slug: string } }) => {
   const teamID = +params.slug.split("-")[1];
   const team = await getTeamByID(teamID).catch(() => {
     notFound();
   });
+  const userID = await getLoggedUserID();
   return (
     <div className="flex min-h-screen w-full flex-col container gap-6 ">
-      <Button className="w-min">Add member</Button>
-
+      <AddMemberDialog teamID={teamID} ownerID={userID} />
       {team?.members.map((member, index) => {
         return (
           <div className="flex flex-col gap-6" key={index}>
@@ -23,7 +33,15 @@ const Page = async ({ params }: { params: { slug: string } }) => {
                 <p className="text-sm text-muted-foreground">{member.email}</p>
               </div>
               <div className="ml-auto font-medium">
-                <Button variant={"secondary"}>Manage</Button>
+                <Select>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Theme" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="member">Member</SelectItem>
+                    <SelectItem value="manager">Manager</SelectItem>
+                  </SelectContent>
+                </Select>
               </div>
             </div>
             {index < team.members.length - 1 && <Separator />}
