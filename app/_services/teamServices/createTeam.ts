0f8bb08bad733cf3aaeb6668942a40d265af584a
Ravvsky <pawel.rawsk@gmail.com>
2024-08-19 23:56:43 +0200

    feat: update createTeam to work with new model

--- app/_services/teamServices/createTeam.ts
+++ app/_services/teamServices/createTeam.ts
@@ -4,12 +4,18 @@ import prisma from "@/app/_utils/prisma";
 
 export async function createTeam(name: string) {
   const userID = await getLoggedUserID();
-  console.log(userID);
   return await prisma.team.create({
     data: {
       name: name,
-      owners: { connect: [{ id: userID }] },
-      members: { connect: [{ id: userID }] },
+      members: {
+        create: [
+          {
+            userId: userID,
+            role: "MEMBER",
+            isInvited: false,
+          },
+        ],
+      },
     },
   });
 }
